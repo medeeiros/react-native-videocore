2fc980fc5fea4821293056d45e4cceb8519f6dfd
orenk86 <oren@panda-os.com>
2017-08-24 16:49:58 +0300

    updated to new React Native syntax

--- index.ios.js
+++ index.ios.js
@@ -4,92 +4,91 @@
  */
 'use strict';
 
-var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  requireNativeComponent,
-  NativeModules,
-  DeviceEventEmitter,
-  TouchableHighlight
-} = React;
+import React, {Component} from 'react';
+import ReactNative, {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    requireNativeComponent,
+    NativeModules,
+    DeviceEventEmitter,
+    TouchableHighlight
+} from 'react-native';
 
 var VideoCoreView = requireNativeComponent('RCTVideoCoreView', null);
 
-var VideoCore = React.createClass({
-  statics: {
-    startStreaming: function(url, key) {
-      NativeModules.VideoCoreViewManager.startStreaming(url, key)
-    },
-
-    stopStreaming: function() {
-      NativeModules.VideoCoreViewManager.stopStreaming()
-    },
-  },
-
-  getInitialState: function() {
-    return {
-      status: 'VCSessionStateNone'
+export class VideoCore extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            status: 'VCSessionStateNone'
+        }
     }
-  },
 
-  componentWillMount: function(){
-    NativeModules.VideoCoreViewManager.stopStreaming()
-    this.connectionStatusChangedListener = DeviceEventEmitter.addListener('videocore.connectionStatusChanged', this._onConnectionStatusChanged)
-  },
 
-  _onConnectionStatusChanged: function(e) {
-    this.setState({
-      status: e
-    })
-    this.props.onConnectionStatusChanged && this.props.onConnectionStatusChanged(e)
-  },
-
-  componentWillUnmount: function(){
-    this.connectionStatusChangedListener.remove();
-    NativeModules.VideoCoreViewManager.stopStreaming()
-  },
-
-  render: function() {
-    return <VideoCoreView {...this.props} />
-  }
-
-})
-
-var RNVideoCore = React.createClass({
-
-  getInitialState: function() {
-    return {
-      showPreview: false
+    componentWillMount() {
+        NativeModules.VideoCoreViewManager.stopStreaming()
+        this.connectionStatusChangedListener = DeviceEventEmitter.addListener('videocore.connectionStatusChanged', this._onConnectionStatusChanged.bind(this))
     }
-  },
-  render: function() {
-    var previewOrButton = (this.state.showPreview) ?
-      <VideoCore style={{ alignSelf: 'stretch', flex: 1 }} onConnectionStatusChanged={(e) => console.log(e)}>
-        <TouchableHighlight onPress={() => VideoCore.startStreaming('rtmp://104.155.71.82:1935/live', 'myStream')}><Text style={{color: "#fff", margin: 50}}>START Streaming</Text></TouchableHighlight>
-        <TouchableHighlight onPress={() => VideoCore.stopStreaming()}><Text style={{color: "#fff", margin: 50}}>STOP Streaming</Text></TouchableHighlight>
-      </VideoCore> :
-      <TouchableHighlight onPress={() => this.setState({showPreview: true})}><Text>Open Streaming</Text></TouchableHighlight>
 
-    return (
-      <View style={styles.container}>
-        {previewOrButton}
-      </View>
-    );
-  }
-});
+    _onConnectionStatusChanged(e) {
+        this.setState({
+            status: e
+        })
+        this.props.onConnectionStatusChanged && this.props.onConnectionStatusChanged(e)
+    }
+
+    componentWillUnmount() {
+        this.connectionStatusChangedListener.remove();
+        NativeModules.VideoCoreViewManager.stopStreaming()
+    }
+
+    render() {
+        return <VideoCoreView {...this.props} />
+    }
+
+    static startStreaming(url, key) {
+        NativeModules.VideoCoreViewManager.startStreaming(url, key)
+    }
+
+    static stopStreaming() {
+        NativeModules.VideoCoreViewManager.stopStreaming()
+    }
+};
+
+
+class RNVideoCore extends Component {
+
+    getInitialState() {
+        return {
+            showPreview: false
+        }
+    }
+    render() {
+        var previewOrButton = (this.state.showPreview) ?
+            <VideoCore style={{ alignSelf: 'stretch', flex: 1 }} onConnectionStatusChanged={(e) => console.log(e)}>
+                <TouchableHighlight onPress={() => VideoCore.startStreaming('rtmp://stream.panda-os.com/pandaLive', 'mp4:0_fdm8zu97')}><Text style={{color: "#fff", margin: 50}}>START Streaming</Text></TouchableHighlight>
+                <TouchableHighlight onPress={() => VideoCore.stopStreaming()}><Text style={{color: "#fff", margin: 50}}>STOP Streaming</Text></TouchableHighlight>
+            </VideoCore> :
+            <TouchableHighlight onPress={() => this.setState({showPreview: true})}><Text>Open Streaming</Text></TouchableHighlight>
+
+        return (
+            <View style={styles.container}>
+                {previewOrButton}
+            </View>
+        );
+    }
+};
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    }
 });
+AppRegistry.registerComponent('VideoCore', () => VideoCore);
 
-// AppRegistry.registerComponent('RNVideoCore', () => RNVideoCore);
-
-module.exports = VideoCore;
+AppRegistry.registerComponent('RNVideoCore', () => RNVideoCore);
